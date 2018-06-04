a344f00ddd93cf2cd89d335879010a3b03f31bc5
Shian <shianyip@gmail.com>
2018-06-04 20:40:12 +0800

    refactor(move L* components to basic folder):

---
+++ src/components/basic/LHeader.vue
@@ -0,0 +1,28 @@
+<template>
+  <el-menu mode="horizontal" default-active="activeIndex" menu-trigger="click"
+           @select="handleSelect" :router="ifRouter"
+           style="border-top: 1px solid #E0E3E6;"
+  >
+    <el-menu-item index="1" route="Workspace">Workspace</el-menu-item>
+    <el-menu-item index="2" route="Tasks">Tasks</el-menu-item>
+    <el-menu-item index="3" route="Script">Script</el-menu-item>
+    <el-menu-item index="4" route="About">About</el-menu-item>
+  </el-menu>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        activeIndex: 1,
+        aboutLink: 'https://www.github.com/ZexuanTHU/llmt/',
+        ifRouter: true,
+      };
+    },
+    methods: {
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+      },
+    },
+  };
+</script>

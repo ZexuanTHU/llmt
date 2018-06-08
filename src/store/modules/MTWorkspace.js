49b8ba97572f2d65f0c16a5f22b5ef1243173814
Shian <shianyip@gmail.com>
2018-06-08 19:31:13 +0800

    feat: add Vuex state moduel: MTWorkspace

---
+++ src/store/modules/MTWorkspace.js
@@ -0,0 +1,33 @@
+const state = {
+  states: [
+    {name: 'GB_NM_PER_PIXEL', value: '1', units: 'nm/pixel'},
+    {name: 'GB_TIME_INTERVAL', value: '100', units: 's'},
+    {name: 'gb_Lmax', value: '', units: ''},
+    {name: 'gb_LmaxF', value: '', units: ''},
+    {name: 'gb_Lmin', value: '', units: ''},
+    {name: 'gb_LminF', value: '', units: ''},
+    {name: 'gb_bead_file_list', value: '', units: ''},
+    {name: 'gb_bead_file_num', value: '', units: ''},
+    {name: 'gb_blurF', value: '', units: ''},
+    {name: 'gb_blurred_mt_imgs', value: '', units: ''},
+    {name: 'gb_imagej_tf_file', value: '', units: ''},
+    {name: 'gb_is_preprocessed', value: '', units: ''},
+    {name: 'gb_map_file', value: '', units: ''},
+    {name: 'gb_map_imgs', value: '', units: ''},
+    {name: 'gb_mt_file', value: '', units: ''},
+    {name: 'gb_mt_imgs', value: '', units: ''},
+    {name: 'gb_mt_lines', value: '', units: ''},
+    {name: 'gb_mt_minus_params', value: '', units: ''},
+    {name: 'gb_mt_plus_params', value: '', units: ''},
+    {name: 'gb_normed_mt_imgs', value: '', units: ''},
+    {name: 'gb_params', value: '', units: ''},
+    {name: 'gb_skel', value: '', units: ''},
+    {name: 'gb_skel_lines', value: '', units: ''},
+    {name: 'gb_tform', value: '', units: ''},
+    {name: 'gb_theta', value: '', units: ''},
+  ],
+};
+
+export default {
+  state,
+};
